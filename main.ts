68527d677d26a023e26a06ef5bf2630f11abebfc
DrMeepso <50252724+DrMeepso@users.noreply.github.com>
2020-09-22 11:31:37 +1200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+let Channel = 0
+input.onPinPressed(TouchPin.P1, function () {
+    Channel = 0
+    basic.clearScreen()
+})
+basic.forever(function () {
+    if (input.pinIsPressed(TouchPin.P0)) {
+        if (Channel <= 255) {
+            led.toggle(2, 2)
+            radio.setGroup(Channel)
+            Channel += 1
+        } else {
+            Channel = 0
+        }
+        radio.sendNumber(69)
+        radio.sendString("intruder")
+        radio.sendString("Int.")
+    }
+})
